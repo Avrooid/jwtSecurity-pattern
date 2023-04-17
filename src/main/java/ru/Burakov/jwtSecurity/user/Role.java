59459dd36d3d46c7e8e408731524015b973c06e7
Nikita <nikitaburakov08@gmail.com>
2023-04-17 20:55:23 +0500

    add jwt-pattern

---
+++ src/main/java/ru/Burakov/jwtSecurity/user/Role.java
@@ -0,0 +1,6 @@
+package ru.Burakov.jwtSecurity.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/test/java/ru/Burakov/jwtSecurity/JwtSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.Burakov.jwtSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
